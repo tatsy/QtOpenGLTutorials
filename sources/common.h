05825382b55493df02d911e4de8cea3616bf97a6
tatsy <tatsy.mail@gmail.com>
2016-06-01 16:56:24 +0900

    Bug fix.

--- sources/common.h
+++ sources/common.h
@@ -5,8 +5,8 @@
 #ifndef _COMMON_H_
 #define _COMMON_H_
 
-static const char* ROOT_DIRECTORY   = "C:/Users/Tatsuya/Desktop/Lab/gshadertest/";
-static const char* SOURCE_DIRECTORY = "C:/Users/Tatsuya/Desktop/Lab/gshadertest/sources/";
-static const char* DATA_DIRECTORY   = "C:/Users/Tatsuya/Desktop/Lab/gshadertest/data/";
+static const char* ROOT_DIRECTORY   = "C:/Users/Tatsuya/Desktop/Lab/qopenglsample/";
+static const char* SOURCE_DIRECTORY = "C:/Users/Tatsuya/Desktop/Lab/qopenglsample/sources/";
+static const char* DATA_DIRECTORY   = "C:/Users/Tatsuya/Desktop/Lab/qopenglsample/data/";
 
 #endif  // _COMMON_H_
