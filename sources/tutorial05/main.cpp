f3cb212578738594d68db290acb77a07a074a2a8
tatsy <tatsy.mail@gmail.com>
2016-07-02 03:43:37 +0900

    Update CMakeFile for MacOSX

--- sources/tutorial05/main.cpp
+++ sources/tutorial05/main.cpp
@@ -1,9 +1,16 @@
 #include <QtWidgets/qapplication.h>
+#include <QtGui/qsurfaceformat.h>
 
 #include "oglwidget.h"
 
 int main(int argc, char **argv) {
     QApplication app(argc, argv);
+    
+    QSurfaceFormat format = QSurfaceFormat::defaultFormat();
+    format.setVersion(3, 3);
+    format.setProfile(QSurfaceFormat::CoreProfile);
+    format.setOption(QSurfaceFormat::DeprecatedFunctions, false);
+    QSurfaceFormat::setDefaultFormat(format);
 
     OGLWidget widget;
     widget.resize(800, 600);
--- sources/tutorial06/main.cpp
+++ sources/tutorial06/main.cpp
@@ -1,9 +1,15 @@
 #include <QtWidgets/qapplication.h>
+#include <QtGui/qsurfaceformat.h>
 
 #include "oglwidget.h"
 
 int main(int argc, char **argv) {
     QApplication app(argc, argv);
+    
+    QSurfaceFormat format = QSurfaceFormat::defaultFormat();
+    format.setVersion(3, 3);
+    format.setProfile(QSurfaceFormat::CoreProfile);
+    QSurfaceFormat::setDefaultFormat(format);
 
     OGLWidget widget;
     widget.resize(800, 600);
